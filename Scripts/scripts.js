b93f61eab3628599fdfa2af7c9bc0200f76882e4
PhatPratt905 <46850622+PhatPratt905@users.noreply.github.com>
2019-01-25 22:54:27 -0500

    Add files via upload

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -14,35 +14,6 @@ if ($(window).width() > 768) {
   }
 }
 
-$(window).resize(function() {
-  if ($(window).width() > 768) {
-    var lastScrollTop = 0;
-    $(document).on('scroll', function () {
-        var st = $(this).scrollTop();
-        if (st > lastScrollTop) {
-            $('#navBar').fadeOut(1000);
-        } else {
-            $('#navBar').fadeIn(1000);
-        }
-        lastScrollTop = st;
-    });
-    function hideNav() {
-      $('#navBar').fadeOut(1000);
-    }
-  }
-});
-
-
-
-
-
-
-
-
-
-
-
-
 function myFunction(x) {
   x.classList.toggle("change");
 }
@@ -53,25 +24,7 @@ $(".a-link").on('click', function() {
   $(".switch").fadeOut(10);
   $("div#"+x).fadeIn(800);
 });
-$(function() {
 
-  // contact form animations
-  $('.formButton').click(function() {
-    $('#email').fadeToggle();
-  })
-  $(document).mouseup(function (e) {
-    var container = $("#email");
-
-    if (!container.is(e.target) // if the target of the click isn't the container...
-        && container.has(e.target).length === 0) // ... nor a descendant of the container
-    {
-        container.fadeOut();
-    }
-  });
-//^^JS piece taken from https://codepen.io/andrewerrico/pen/Efyrt THANK YOU!
-
-
-});
 function thankYou() {
     alert("Message has been sent, Thank you!");
 }
@@ -88,16 +41,4 @@ $(function() {
     $('.a-link').removeClass("active");
     $(this).addClass("active");
   })
-});
-
-
-
-$(window).on('resize', function() {
-    if (!eventFired) {
-        if ($(window).width() < 960) {
-            alert('Less than 960 resize');
-        } else {
-            alert('More than 960 resize');
-        }
-    }
 });
